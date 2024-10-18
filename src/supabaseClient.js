f32f330aadea2a4a15b94e90e5d4446ea8ca23a4
soorajydv <soorajydv9@gmail.com>
2024-10-18 18:51:49 +0545

    env setup

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,11 +1,7 @@
-// src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
+require('dotenv').config();
 
-const supabaseUrl = 'https://xjvuijgnsmzxbfjerdal.supabase.co'; // Replace with your Supabase URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqdnVpamduc216eGJmamVyZGFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkxNTYwODgsImV4cCI6MjA0NDczMjA4OH0.kBR84hcfv9RBXDb5XIuDWUUWFbUE3lkWG6U0onpRM5s'; // Replace with your Supabase anon key
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.SUPABASE_ANNON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-// REACT_APP_SUPABASE_URL=https://xjvuijgnsmzxbfjerdal.supabase.co
-// REACT_APP_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqdnVpamduc216eGJmamVyZGFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkxNTYwODgsImV4cCI6MjA0NDczMjA4OH0.kBR84hcfv9RBXDb5XIuDWUUWFbUE3lkWG6U0onpRM5s
-        
